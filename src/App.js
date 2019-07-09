063cb7dc2deb0e74219fbb437c1f2a36f52d511f
github username <crishanks12@gmail.com>
2019-07-09 10:18:48 -0600

    create Home component, added it to route in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
 
+//React Router
+import { Switch, Route } from 'react-router-dom';
+
+//Components
+import Home from '../src/containers/Home';
+
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path='/' component={Home}/>
+      </Switch>
     </div>
   );
 }
