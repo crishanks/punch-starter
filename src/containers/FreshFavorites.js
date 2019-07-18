257473c15dec59a5f7f8c549e35d163dce126e96
github username <crishanks12@gmail.com>
2019-07-17 18:35:03 -0600

    fresh favorites basic layout applied

--- src/containers/FreshFavorites.js
+++ src/containers/FreshFavorites.js
@@ -11,7 +11,7 @@ export default class FreshFavorites extends Component {
 
   render() {
     return (
-      <div>
+      <>
         <SectionHeader header={"fresh favorites"}/>
         <ProjectPreview 
           image={"https://ksr-ugc.imgix.net/assets/025/775/780/cdfd71d86476b5994cb6ff2aaa8955af_original.png?ixlib=rb-2.1.0&crop=faces&w=352&h=198&fit=crop&v=1562861681&auto=format&frame=1&q=92&s=413107d31d9312c08a85a2461e77e9ba"}
@@ -37,7 +37,7 @@ export default class FreshFavorites extends Component {
           description={"A series of three existential, experimental games inpired by color and dreams. They don't exist yet. I trust you. Do you trust me?"}
           authorName={"Nathan D. Paoletta"}
         />
-      </div>
+      </>
     );
   }
 }
