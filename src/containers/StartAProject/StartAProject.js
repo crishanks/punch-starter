b4e86ec0e6b44a71d5039a24dd800a523b33bf20
github username <crishanks12@gmail.com>
2019-08-06 13:24:30 -0600

    page number added, added pageNumber state, adjusted grid layout

--- src/containers/StartAProject/StartAProject.js
+++ src/containers/StartAProject/StartAProject.js
@@ -8,20 +8,25 @@ import NextContainer from './NextContainer';
 
 const StartAProject = () => {
   const [dropDownClicked, toggleDropDownClick] = useState(false);
+  const [pageNumber, setPageNumber] = useState(1);
+
   return (
     <div className="start-a-project">
-        <PreviewTitle title={"Let's get you set up."}/>
-        <ShortDescription description={"Pick a project category to connect with a specific community. You can always update this later."}/>
-        <div className="drop-down-header" onClick={() => toggleDropDownClick(!dropDownClicked)}>
-          <DropDownMenu 
-            title={"Select your category"}
-            dropDownClicked={dropDownClicked}
-            toggleDropDownClick={toggleDropDownClick}
-          />
-        </div>
-        <div className="next-container">
-          <NextContainer blurb="Your first project! Welcome."/>
-        </div>
+      <div className="page-number">
+        <p>{pageNumber} of 3</p>
+      </div>
+      <PreviewTitle title={"Let's get you set up."}/>
+      <ShortDescription description={"Pick a project category to connect with a specific community. You can always update this later."}/>
+      <div className="drop-down-header" onClick={() => toggleDropDownClick(!dropDownClicked)}>
+        <DropDownMenu 
+          title={"Select your category"}
+          dropDownClicked={dropDownClicked}
+          toggleDropDownClick={toggleDropDownClick}
+        />
+      </div>
+      <div className="next-container">
+        <NextContainer blurb="Your first project! Welcome."/>
+      </div>
     </div>
   );
 }
