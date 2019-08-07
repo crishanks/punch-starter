2d04d2cc449978f96ba9441870369c5de468ea33
github username <crishanks12@gmail.com>
2019-08-06 20:30:58 -0600

    next button click updates page number state

--- src/components/Home/SquareButton.js
+++ src/components/Home/SquareButton.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const SquareButton = props => {
-  console.log('button props: ', props)
   return (
     <div className="square-button">{props.title}</div>
   );
--- src/containers/StartAProject/NextContainer.js
+++ src/containers/StartAProject/NextContainer.js
@@ -4,16 +4,22 @@ import React, { Component } from 'react';
 import SquareButton from '../../components/Home/SquareButton';
 
 const NextContainer = (props) => {
+  console.log('next container props: ', props)
   return (
     <>
       <div className="next-container-blurb">
         <p>{props.blurb}</p>
       </div>
-      <div className="button-container" onClick={(props) => props.setPageNumber(props.pageNumber++)}>
+      <div className="button-container" onClick={() => props.setPageNumber(updatePageNumber(props))}>
         <SquareButton title={"Next: Project idea"} />
       </div>
     </>
   );
+}
+
+const updatePageNumber = (props) => {
+  let currentPageNumber = props.pageNumber;
+  return currentPageNumber >= 3 ? currentPageNumber : ++currentPageNumber;
 }
 
 export default NextContainer;
