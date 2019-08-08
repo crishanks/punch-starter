4c99e531626cb020c10be251c5d74f1fa230972d
github username <crishanks12@gmail.com>
2019-08-08 15:47:14 -0600

    square button links now 100% width and height for fully clickable divs

--- src/components/Home/SquareButton.js
+++ src/components/Home/SquareButton.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const SquareButton = props => {
-  return props.link ? <div className="square-button"><a href={props.link}>{props.title}</a></div> : <div className="square-button">{props.title}</div>
+  return props.link ? 
+    <div className="square-button">
+      <a href={props.link}>{props.title}</a>
+    </div> 
+    : 
+    <div className="square-button">{props.title}</div>
 }
 
 export default SquareButton;
